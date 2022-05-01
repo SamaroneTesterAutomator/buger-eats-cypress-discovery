45fd2e27f9e7924f5fd1d4eb90fd30aee05792b0
= <=>
2022-05-01 01:25:08 -0300

    Suite home comentada

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,8 +1,8 @@
 
 
-describe('Home Page', ()=>{
-    it('app deve estar online', ()=>{
-        cy.visit('/')
-        cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
-    })
-})
+// describe('Home Page', ()=>{
+//     it('app deve estar online', ()=>{
+//         cy.visit('/')
+//         cy.get('#page-home main h1').should('have.text', 'Seja um parceiro entregador pela Buger Eats')
+//     })
+// })
